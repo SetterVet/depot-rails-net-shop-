da62d4116d11eb00c8d95da279c19892a0c1a247
SetterVet <kosvit1408@gmail.com>
2018-12-05 21:59:01 +0200

    Add ajax to add product in cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -57,7 +57,7 @@ class CartsController < ApplicationController
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to store_url, notice: 'Your cart already empty' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
@@ -65,7 +65,11 @@ class CartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
-      @cart = Cart.find(params[:id])
+      begin
+        @cart = Cart.find(params[:id])
+
+
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
